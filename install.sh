1788ecb2f717b505a99065f8470691d174adc700
SuCicada <pengyifu@gmail.com>
2021-12-13 11:14:45 +0800

    Mon Dec 13 11:14:45 CST 2021

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ python3 -m pip install -r "$current"/pip.txt
 
 conf_dir=~/etc/supervisor/conf.d/
 test -d $conf_dir || mkdir -p $conf_dir
-sed -i "s/\${deploy.path}/$current/" moehira.conf
 
-cp "$current"/moehira.conf $conf_dir
+cp "$current"/moehira.conf $conf_dir/moehira.conf
+
+sed -i "s/\${deploy.path}/$current/" $conf_dir/moehira.conf
 
 supervisorctl reread
 supervisorctl restart moehira
