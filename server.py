a82a5e0ffdb9372a72bc19cc81f19e1d0546bd72
SuCicada <pengyifu@gmail.com>
2021-12-17 11:47:14 +0800

    support request.args

--- server.py
+++ server.py
@@ -6,16 +6,27 @@ import moehira
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 # 跨域支持
+
+
 def after_request(resp):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
+
 app.after_request(after_request)
 CORS(app, supports_credentials=True)
 
 
 @app.route('/', methods=['POST'])
 def tran():
-    orig = request.json.get("orig")
+    if request.json is not None:
+        orig = request.json.get("orig")
+    elif request.args is not None:
+        orig = request.args.get("orig")
+    else:
+        print("no data")
+        return None
+
     orig_list = orig.split("\n")
     print("orig", orig_list)
     hira_list = moehira.kanji2moe(orig_list)
@@ -23,6 +34,7 @@ def tran():
     print("res", hira_list)
     print("res", res)
     return {"hira": res}
+
 
 if __name__ == '__main__':
     app.run(
