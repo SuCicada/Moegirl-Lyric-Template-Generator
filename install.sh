85aa7c7cad22ea72e50d0b3c2b3a68acec939d70
SuCicada <pengyifu@gmail.com>
2022-04-21 16:46:12 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ cp "$current"/deploy/moehira.conf $conf_dir/moehira.conf
 
 sed -i "s|\${deploy.path}|$current|g" $conf_dir/moehira.conf
 
-supervisorctl update
-supervisorctl restart moehira
+~/bin/supervisorctl update
+~/bin/supervisorctl restart moehira
