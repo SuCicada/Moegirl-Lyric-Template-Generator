1433d36a90d4f0764721b782889bc2f3ebc1dae6
SuCicada <pengyifu@gmail.com>
2022-07-23 23:14:46 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ python3 -m pip install -r "$current"/server/pip.txt
 conf_dir=~/etc/supervisor/conf.d/
 test -d $conf_dir || mkdir -p $conf_dir
 
-cp "$current"/deploy/moehira.conf $conf_dir/moehira.ini
+cp "$current"/deploy/moehira.ini $conf_dir/moehira.ini
 
 sed -i "s|\${deploy.path}|$current|g" $conf_dir/moehira.ini
 
