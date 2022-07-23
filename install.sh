8cc9aca02479c83731ce176979df696987e15548
SuCicada <pengyifu@gmail.com>
2022-07-23 23:01:52 +0800

    [modify]切换ip

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ python3 -m pip install -r "$current"/server/pip.txt
 conf_dir=~/etc/supervisor/conf.d/
 test -d $conf_dir || mkdir -p $conf_dir
 
-cp "$current"/deploy/moehira.conf $conf_dir/moehira.conf
+cp "$current"/deploy/moehira.conf $conf_dir/moehira.ini
 
-sed -i "s|\${deploy.path}|$current|g" $conf_dir/moehira.conf
+sed -i "s|\${deploy.path}|$current|g" $conf_dir/moehira.ini
 
 ~/bin/supervisorctl.sh update
 ~/bin/supervisorctl.sh restart moehira
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC2155
 
 export FLASK_APP=$(dirname "$0")/server/server.py
-python3 -u -m flask run -h 0.0.0.0 -p 41401
+python3 -u -m flask run -h 0.0.0.0 -p 41403
